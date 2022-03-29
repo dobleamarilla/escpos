1efd23f80f067938135bacb6005be6455a9fdbbc
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-03-29 14:04:10 +0200

    Test 6
    Ya imprime

--- dist/impresora/impresora.js
+++ dist/impresora/impresora.js
@@ -5,6 +5,33 @@ const dispositivos_1 = require("../dispositivos");
 const escpos = require('escpos');
 escpos.USB = require('escpos-usb');
 const dispositivos = new dispositivos_1.Dispositivos();
+const test = `
+{C:27}{C:97}{C:0}\n
+!IJoan Cabre Saborit\n
+!ANIF:39824549A\n
+Pere III, 14 - 43850 Cambrils\n
+TEL:977361557\n
+FAX:977363425\n
+!9!9   ALBARA ENTREGA\n
+BAR LA  VIA\n
+\n";
+!BNIF          : B55696355\n
+Data         : Dimarts 29-03-2022\n
+Adre‡a       : SEGRE,2\n
+CP           : 43850\n
+Tlf          : \n
+Num albar…   : 15744\n
+!B!B\n
+Article                            TORNAT     Servit\n
+!A________________________________________________________\n
+!BBARRA 1/4                                       15\n
+CROISSANT GRAN                                   3\n
+________________________________________________________\n
+\n
+VB\n
+\n
+\n\n\n{C:29}V{C:0}
+`;
 class Impresora {
     imprimir() {
         dispositivos.getDevice().then((device) => {
@@ -17,7 +44,7 @@ class Impresora {
                         .style('b')
                         .align('CT')
                         .size(0, 0)
-                        .text("HOLA HOLITA")
+                        .text(test)
                         .cut()
                         .close();
                 });
